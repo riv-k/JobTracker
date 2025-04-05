abd343f2541b76b3022255f1834e2113e4c307ca
Rithvik Sharma <rsha264@aucklanduni.ac.nz>
2025-04-05 23:44:06 +1300

    split up css files to maintainability. Desgined popupp modal, need to implement.

--- JobTracker/Models/JobApplication.cs
+++ JobTracker/Models/JobApplication.cs
@@ -9,22 +9,24 @@ namespace JobTracker.Models;
 public class JobApplication
 {
     public string CompanyName { get; set; }
-    public string RoleName { get; set; }
+    public string JobTitle { get; set; }
     public string Description { get; set; }
     public string Status { get; set; } // "Interested", "Applied", etc.
     public DateTime DateApplied { get; set; }
     public string Link { get; set; } // Link to job posting
     public string ResumePath { get; set; } // Path to uploaded resume or cover letter
+    public string Location { get; set; }
 
     // Constructor to initialize job application
-    public JobApplication(string companyName, string roleName, string description, string status, DateTime dateApplied, string link, string resumePath)
+    public JobApplication(string companyName, string jobTitle, string description, string status, DateTime dateApplied, string link, string resumePath, string location)
     {
         CompanyName = companyName;
-        RoleName = roleName;
+        JobTitle = jobTitle;
         Description = description;
         Status = status;
         DateApplied = dateApplied;
         Link = link;
         ResumePath = resumePath;
+        Location = location;
     }
 }
